33965b000a23c32c76ae15fce69b7dad2d949ee0
Gianfranco Vaccaro <gianv322@gmail.com>
2021-12-15 14:41:00 -0500

    commit tree 152c49efbfd3db08b14cc3157966ea4a953144df

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+if (distance <= 400) {
+  return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  if (city = NYC){
+    return ('Ok, sounds good.')}
+  else (_city=" ")
+    return ('No go.')
+  }
+  }
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
